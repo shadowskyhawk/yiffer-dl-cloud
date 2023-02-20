873df8f10b511a848ebe23844f89da94bf9040fc
shadowskyhawk <125842891+shadowskyhawk@users.noreply.github.com>
2023-02-19 18:56:24 -0800

    Update ydl.py
    Initial update to include cloud support

--- ydl.py
+++ ydl.py
@@ -7,10 +7,12 @@ from sys import argv
 import shutil
 from posixpath import join as urljoin
 from zipfile import ZipFile
+import owncloud
+import config
 
 # Constants
 U_HOME = os.path.expanduser("~")
-DL_LOC = os.path.join(U_HOME, "ydl/")
+DL_LOC = os.path.join(U_HOME, config.downloadFolder)
 
 # Input
 if len(argv) < 2:
@@ -24,7 +26,7 @@ def download_comic(name, DL_LOC):
     err = None
 
     # Get comic name and create dir in dl/
-    cname = name.replace(" ", "_")
+    cname = name.replace(" ", config.space)
     cname_url = name.replace(" ", "%20")
 
     # Create download folder based on comic name
@@ -33,9 +35,9 @@ def download_comic(name, DL_LOC):
     except:
         print(f"Comic directory already exists: '{cname}'")
     try:
-        os.mkdir(os.path.join(DL_LOC, cname, "Imgs"))
+        os.mkdir(os.path.join(DL_LOC, cname, "Images"))
     except:
-        print(f"Comic directory already exists: '{cname}/Imgs'")
+        print(f"Comic directory already exists: '{cname}/Images'")
     
     # Get comic info from API (length, url)
     cname_api_url = ("https://yiffer.xyz/api/comics/"+cname_url)
@@ -53,25 +55,26 @@ def download_comic(name, DL_LOC):
         if img_id > comic_length:
             print("=> Done downloading...")
             zip_files(DL_LOC, cname)
+            cloud_upload(DL_LOC, cname, config)
             break
 
 def download_image(DL_LOC, cname, img_id):
     # Parse URL
     img_url = urljoin(
         "comics/", 
-        cname.replace("_", "%20"), 
+        cname.replace(" ", "%20"), 
         f"00{img_id}.jpg"[-7:])
     dl_url = urljoin("https://static.yiffer.xyz/", img_url)
 
     # Check image exists
-    if os.path.isfile(os.path.join(DL_LOC, cname, "Imgs") + "/" + f"00{img_id}.jpg"[-7:]):
+    if os.path.isfile(os.path.join(DL_LOC, cname, "Images") + "/" + f"00{img_id}.jpg"[-7:]):
         print(f"Image already exists: '{img_id}.jpg'")
         return None
 
     # Download image
     res = requests.get(dl_url, stream = True)
     if res.status_code == 200:
-        with open(os.path.join(DL_LOC, cname, "Imgs") + "/" + f"00{img_id}.jpg"[-7:], 'wb') as f:
+        with open(os.path.join(DL_LOC, cname, "Images") + "/" + f"00{img_id}.jpg"[-7:], 'wb') as f:
             res.raw.decode_content = True
             shutil.copyfileobj(res.raw, f)
         print("Downloaded image " + f"00{img_id}"[-3:])
@@ -81,11 +84,22 @@ def download_image(DL_LOC, cname, img_id):
 
 def zip_files(DL_LOC, cname):
     print("=> Zipping files...")
-    if os.path.isfile(os.path.join(DL_LOC, cname) + f"/{cname}.zip"):
-        print(f"Zip already exists: '{cname}.zip'")
+    if os.path.isfile(config.archiveFolder + f"/{cname}.zip"):
+        print(f"Comic file already exists: '{cname}.zip'")
         print("=> Done!")
         return None
-    shutil.make_archive(os.path.join(DL_LOC, cname) + f"/{cname}", "zip", os.path.join(DL_LOC, cname, "Imgs"))
+    shutil.make_archive(config.archiveFolder + f"/{cname}", "zip", os.path.join(DL_LOC, cname, "Images"))
+    print("=> Done!")
+
+def cloud_upload(DL_LOC, cname, config):
+    # Login to Nextcloud/Owncloud
+    print("=> Logging in to cloud server")
+    oc = owncloud.Client(config.server)
+    oc.login(config.username, config.password)
+    print("=> Done!")
+    print("=> Uploading {format}...")
+    # upload file
+    oc.put_file(config.uploadFolder + f"/{cname}.{config.uploadFormat}", os.path.join(DL_LOC, cname) + f"/{cname}.zip")
     print("=> Done!")
 
 if __name__ == "__main__":
