341fd8f1175972cdc6d97016e539f4edcd4ad9a0
shadowskyhawk <idontwantyourstupidnewsletter@hotmil.com>
2023-02-19 20:58:40 -0800

    Updated script

---
+++ config.py
@@ -0,0 +1,23 @@
+# Local download folder
+downloadFolder = "~/yiffer-dl/images"
+archiveFolder = "~/yiffer-dl/comics"
+
+# Your cloud credentials
+username = "test"
+password = "pass123"
+server = "https://cloud.example.com/"
+uploadFolder = "Yiff/Comics"
+
+# Format of uploaded archive. Options are cbz or zip.
+uploadFormat = "zip"
+
+# The following line configures how spaces are handled. By default
+# spaces in the filenames are replaced with underscores, but you can
+# replace them with a space, a dash, or anything you prefer.
+space = "_"
+
+# Unused but planned options
+upload = "y"
+archive = "y"
+imageUpload = "n"
+archiveUpload = "y"
